c3991a886855b747fe67eaafa5a4d26527ed503e
harshvardhan-wagh <harshwagh00@gmail.com>
2025-08-15 10:48:02 +0530

    feat: add system resource viewer
    This new component displays real-time information for CPU, RAM, and storage usage, providing a quick overview of system performance.

--- resourceManager.js
+++ resourceManager.js
@@ -1,13 +1,30 @@
 import osUtils from 'os-utils';
+import fs from "fs";
 const POLLING_INTERVAL = 500;
+// Getting CPU Usage 
 export function pollResource() {
     setInterval(async () => {
         const cpuUsage = await getCpuUsage();
-        console.log(cpuUsage);
+        const ramUsage = await getRamUsage();
+        const storageDate = getStorageData();
+        console.log({ cpuUsage, ramUsage, storageUsage: storageDate.usage });
     }, POLLING_INTERVAL);
 }
 function getCpuUsage() {
     return new Promise((resolve) => {
         osUtils.cpuUsage(resolve);
     });
+}
+function getRamUsage() {
+    return 1 - osUtils.freememPercentage();
+}
+function getStorageData() {
+    // requires node 18
+    const stats = fs.statfsSync(process.platform === 'win32' ? 'C://' : '/');
+    const total = stats.bsize * stats.blocks;
+    const free = stats.bsize * stats.bfree;
+    return {
+        total: Math.floor(total / 1_000_000_000),
+        usage: 1 - free / total,
+    };
 }
